36a539aa47744eeaec7f666395ab1e49fe4e12ce
Roberto Ramirez <42901082+robertpiscis2@users.noreply.github.com>
2020-07-19 23:58:55 -0500

    commit_19/07/2020

---
+++ js/my-scripts/script-login-fetch.js
@@ -0,0 +1,40 @@
+let login = document.getElementById('btnSubmitLogin')
+login.addEventListener('click', submitDataPost)
+
+let url = 'https://truequemundov1.herokuapp.com/login'
+
+console.log(url)
+
+// fetch(url)
+//   .then(function(response) {
+//     return response.json()
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson)
+//   });
+
+function submitDataPost(){
+    let email = document.querySelector('#frmEmailLogin')
+    let password = document.querySelector('#frmPasswordLogin')
+
+    var data = {
+        "email": email.value,
+        "password": password.value
+    };
+
+    console.log(data.email + data.password)
+    debugger
+
+    fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+        'Content-Type': 'application/json'
+    }
+    }).then(res => {
+        let response = res.json()
+        console.log(response);
+    })
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+}
